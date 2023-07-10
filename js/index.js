7fb00162fd72296cf75f9713c29a0f8a9ccd802a
fabian parra <fabi_8931@hotmail.com>
2023-07-10 01:06:22 -0500

    adicion panl admin, confiramr pedido, eliminar pedido

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ let confirmaPedido = false;
 let cartaEntradas = '';
 let arrayPedido = JSON.parse(localStorage.getItem("pedido")) || [];
 let idProducto = 1;
+// let idPedido = 1;
 
 const arrayCartaRestaurante = [];
 
@@ -16,8 +17,6 @@ class Producto {
         this.src = src;
     }
 }
-
-
 
 const entrada1 = new Producto(idProducto++, "entrada", "CANASTA DE EMPANADAS", 8000, "./img/empanadas.png");
 arrayCartaRestaurante.push(entrada1);
@@ -40,24 +39,59 @@ arrayCartaRestaurante.push(bebida2);
 const bebida3 = new Producto(idProducto++, "bebida", "CERVEZA", 5000, "./img/cerveza.png");
 arrayCartaRestaurante.push(bebida3);
 
-console.log(arrayCartaRestaurante);
+cantidadProductosPedido(arrayPedido);
+renderValorPedido(arrayPedido);
 
 
-function renderProductos(contenedor, tipoProducto) {
-    let pedido = false;
-    let arrayProductos
-    if (tipoProducto === "pedido") {
-        pedido = true;
-        arrayProductos = arrayPedido;
-    } else {
-        arrayProductos = filtroCartaProducto(arrayCartaRestaurante, tipoProducto);
-    }
+
+const buttonEntrada = document.querySelector("#button-entrada");
+buttonEntrada.addEventListener("click", () => {
+    // app.innerHTML = "";
+    selectProductos("entrada");
+})
+
+const buttonRapidas = document.querySelector("#button-rapida");
+buttonRapidas.addEventListener("click", () => {
+    // app.innerHTML = "";
+    selectProductos("rapida");
+
+})
+
+const buttonBebidas = document.querySelector("#button-bebida");
+buttonBebidas.addEventListener("click", () => {
+    // app.innerHTML = "";
+    selectProductos("bebida");
+})
+
+const buttonPedido = document.querySelector("#ver-Pedido");
+buttonPedido.addEventListener("click", () => {
+    // app.innerHTML = "";
+    selectProductos("pedido");
+})
+
+const buttonConfirma = document.querySelector("#confirma-pedido");
+buttonConfirma.addEventListener("click", () => {
+    // app.innerHTML = "";
+    renderPedido(arrayPedido);
+    arrayPedido = [];
+    cantidadProductosPedido(arrayPedido);
+    renderValorPedido(arrayPedido);
+    localStorage.removeItem("pedido");
+})
+
+
+function renderProductos(tipoProducto, arrayProductos, isPedido) {
+
+    const contenedorTitulo = document.querySelector("#tituloTipoProducto");
+    const contenedorProductos = document.querySelector("#app");
+    contenedorProductos.innerHTML = "";
+    contenedorTitulo.innerHTML = "";
 
     const tituloTipoProducto = document.createElement("h1");
     tituloTipoProducto.classList.add("titulo-contenedor");
     tituloTipoProducto.innerText = tipoProducto.toUpperCase() + "S";
 
-    contenedor.appendChild(tituloTipoProducto);
+    contenedorTitulo.appendChild(tituloTipoProducto);
     arrayProductos.forEach(element => {
         const tarjeta = document.createElement("div");
         tarjeta.classList.add("tarjeta");
@@ -68,161 +102,107 @@ function renderProductos(contenedor, tipoProducto) {
                         <span> $${element.precio}</span>
                     </div>        
         `
-        
+
         const buttonCard = document.createElement("button");
         buttonCard.classList.add("button-card");
-        pedido ? buttonCard.innerText = "Eliminar del pedido": buttonCard.innerText = "Agregar al pedido";
+        isPedido ? buttonCard.innerText = "Eliminar del pedido" : buttonCard.innerText = "Agregar al pedido";
         buttonCard.addEventListener("click", () => {
-
-            pedido ? eliminarPedido() : agregarPedido(element);
-           
+            isPedido ? eliminarPedido(element, arrayPedido) : agregarPedido(element);
         })
 
-
         tarjeta.appendChild(buttonCard);
-        contenedor.appendChild(tarjeta);
+        contenedorProductos.appendChild(tarjeta);
+
     });
 }
 
-const app = document.querySelector("#app");
-const buttonEntrada = document.querySelector("#button-entrada");
+function renderPedido(arrayPedido) {
+    const contenedorTitulo = document.querySelector("#tituloTipoProducto");
+    const contenedorProductos = document.querySelector("#app");
+    contenedorProductos.innerHTML = "";
+    contenedorTitulo.innerHTML = "";
 
-buttonEntrada.addEventListener("click", () => {
-    app.innerHTML = "";
-    renderProductos(app, "entrada")
-})
+    const tituloTipoProducto = document.createElement("P");
+    tituloTipoProducto.classList.add("titulo-contenedor");
 
-const buttonRapidas = document.querySelector("#button-rapida");
-buttonRapidas.addEventListener("click", () => {
-    app.innerHTML = "";
-    renderProductos(app, "rapida")
-})
+    if (arrayPedido.length > 0) {
+        console.log("Array pedido True")
+    }else{
+        console.log("Array pedido False")
+    }
+    arrayPedido.length > 0 ?  tituloTipoProducto.innerText = `Su pedido se confirmo correctamente, el valor total a pagar es de ${sumarPedido(arrayPedido)}`: tituloTipoProducto.innerText = `Estimado cliente usted aun no ha realizado ningun pedido`;
+    
+    contenedorTitulo.appendChild(tituloTipoProducto);
+    console.log(arrayPedido);
 
-const buttonBebidas = document.querySelector("#button-bebida");
-buttonBebidas.addEventListener("click", () => {
-    app.innerHTML = "";
-    renderProductos(app, "bebida")
-})
+    arrayPedido.forEach(element => {
+        const productosConfirma = document.createElement("div");
+        productosConfirma.classList.add("container", "text-center", "container-producto");
+        productosConfirma.innerHTML = `
+                        <div class="row align-items-end">
+                            <div class="col-3 confirma-divImg">
+                                <img src="${element.src}" alt="${element.producto}">
+                            </div>
+                            <div class="col-4 confirma-divProducto">
+                                <span>${element.producto}</span>
+                            </div>
+                            <div class="col-5 confirma-divPrecio">
+                                <span>$${element.precio}</span>
+                            </div>
+                        </div>      
+        `
+        contenedorProductos.appendChild(productosConfirma);
+    })
 
-const buttonPedido = document.querySelector("#ver-Pedido");
-buttonPedido.addEventListener("click", () => {
-    app.innerHTML = "";
-    renderProductos(app, "pedido");
-})
+    const buttonVolver = document.createElement("div");
+    buttonVolver.innerHTML = `<a class= "button-volver" href="./index.html" target="_blank">Volver</a>`
+    contenedorProductos.appendChild(buttonVolver);
+}
 
-
-// menuInicial();
-// if (confirmaPedido) {
-//     alert("Su pedido se confirmo correctamente el valor a pagar es de $" + sumarPedido(pedido) + " COP");
-// } else {
-//     alert("Gracias por utilizar nuestros servicios");
-// }
-
-
-// function menuInicial() {
-//     while (opcionBienvenida !== 9) {
-//         opcionBienvenida = parseInt(prompt("Bienvenido a su restaurante-Bar \n seleccione la opcion que desea realizar\n 1.Agregar al pedido\n 2.Confirmar pedido \n 3.Modificar pedido\n 9.salir"));
-//         switch (opcionBienvenida) {
-//             case 1:
-//                 agregarPedido();
-//                 break;
-//             case 2:
-//                 confirmaPedido = confirmarPedido();
-//                 break;
-//             case 3:
-//                 modificarPedido();
-//                 break;
-//             default:
-//                 if (opcionBienvenida !== 9) {
-//                     alert("estimado usuari@ por favor Seleccione una opcion valida");
-//                 }
-//                 break;
-//         }
-
-//         if (confirmaPedido) {
-//             break;
-//         }
-//     }
-// }
+function selectProductos(tipoProducto) {
+    let pedido = false;
+    let arrayProductos
+    if (tipoProducto === "pedido") {
+        pedido = true;
+        arrayProductos = arrayPedido;
+    } else {
+        arrayProductos = filtroCartaProducto(arrayCartaRestaurante, tipoProducto);
+    }
+    renderProductos(tipoProducto, arrayProductos, pedido);
+}
 
 function agregarPedido(producto) {
+    // producto.idPedido = idPedido++;
     arrayPedido.push(producto);
     localStorage.setItem("pedido", JSON.stringify(arrayPedido))
-
-    // let opcionTipoProducto = parseInt(prompt(
-    //     "Seleccione la opcion de la cual desea realizar el pedido \n" +
-    //     "1.Entradas \n" +
-    //     "2.Comidas rapidas \n" +
-    //     "3.Bebidas \n"
-    // ));
-
-    // switch (opcionTipoProducto) {
-    //     case 1:
-    //         let entradas = productos(cartaRestaurante, 'entrada');
-    //         carta = filtroCartaProducto(cartaRestaurante, 'entrada', 'MENU DE ENTRADAS');
-    //         let opcionEntrada = parseInt(prompt(carta));
-    //         hacerPedido(entradas, opcionEntrada);
-
-    //         break;
-    //     case 2:
-    //         let comidasRapidas = productos(cartaRestaurante, 'rapida');
-    //         carta = filtroCartaProducto(cartaRestaurante, 'rapida', 'MENU DE COMIDAS RAPIDAS');
-    //         let opcionComidasRapidas = parseInt(prompt(carta));
-    //         hacerPedido(comidasRapidas, opcionComidasRapidas);
-    //         break;
-
-    //     case 3:
-    //         let bebidas = productos(cartaRestaurante, 'bebida');
-    //         carta = filtroCartaProducto(cartaRestaurante, 'bebida', 'MENU DE BEBIDAS');
-    //         let opcionbebidas = parseInt(prompt(carta));
-    //         hacerPedido(bebidas, opcionbebidas);
-    //         break;
-    //     default:
-    //         alert("estimado usuari@ por favor Seleccione una opcion valida");
-    //         agregarPedido();
-    //         break;
-    // }
-
+    cantidadProductosPedido(arrayPedido);
+    renderValorPedido(arrayPedido);
 }
 
-function confirmarPedido() {
-    let _confirmoPedido = false;
-    const precioPedido = sumarPedido(pedido);
-
-    if (pedido.length > 0) {
-        let productosPedidos = 'PEDIDO REALIZADO \n'
-        while (!_confirmoPedido) {
-
-            for (let i = 0; i < pedido.length; i++) {
-                productosPedidos = productosPedidos + pedido[i].producto + ' ............ ' + pedido[i].precio + '\n'
-            }
-            productosPedidos = productosPedidos + 'Costo Total ................... ' + precioPedido + '\n'
-            opcionConfirmar = parseInt(prompt(productosPedidos +
-                "\n Desea confirmar el pedido \n" +
-                "1.Si \n" +
-                "2.No "));
-            switch (opcionConfirmar) {
-                case 1:
-                    _confirmoPedido = true;
-                    break;
-                case 2:
-                    _confirmoPedido = false;
-                    break;
-                default:
-                    alert("estimado usuari@ por favor Seleccione una opcion valida");
-                    break;
-            }
-            if (opcionConfirmar === 2) {
-                break;
-            }
-        }
-        return _confirmoPedido;
-
-    } else {
-        _confirmoPedido = false;
-        alert("estimado usuari@, usted no ha realizado ningun pedido aun");
-    }
+function eliminarPedido(prodcuto, arrayPedido) {
+    console.log(prodcuto);
+    let index = arrayPedido.indexOf(prodcuto);
+    console.log(index);
+    index != -1 && arrayPedido.splice(index, 1);
+    localStorage.removeItem("pedido");
+    localStorage.setItem("pedido", JSON.stringify(arrayPedido));
+    selectProductos("pedido");
+    cantidadProductosPedido(arrayPedido);
+    renderValorPedido(arrayPedido);
 }
+
+function cantidadProductosPedido(arrayPedido) {
+    const cantidadProductos = document.querySelector("#cantidad-productos");
+    cantidadProductos.innerHTML = "";
+    cantidadProductos.innerText = arrayPedido.length;
+}
+
+function renderValorPedido(arrayPedido) {
+    const valorPedido = document.querySelector("#valor-pedido");
+    valorPedido.innerHTML = "";
+    valorPedido.innerHTML = "$" + sumarPedido(arrayPedido);
+}
+
 
 function sumarPedido(arrayPedido) {
     const precioPedido = arrayPedido.reduce((prev, current) => prev + current.precio, 0);
@@ -230,67 +210,12 @@ function sumarPedido(arrayPedido) {
 }
 
 
-function productos(array, tipoProducto) {
-    const productos = array.filter(
-        (el) => {
-            return el.tipoProducto === tipoProducto;
-        }
-    )
-    return productos
-}
-
 function filtroCartaProducto(array, tipoProducto) {
     const cartaTipoProdFiltrado = array.filter(
         (el) => {
             return el.tipoProducto === tipoProducto;
         }
     )
-    // let cartaProductos = tituloTipoProducto + '\n';
-    // for (let i = 0; i < cartaTipoProdFiltrado.length; i++) {
-    //     cartaProductos = cartaProductos + (i + 1) + '-' + cartaTipoProdFiltrado[i].producto + '........................' + cartaTipoProdFiltrado[i].precio + '\n';
-    // }
     return cartaTipoProdFiltrado;
 }
 
-function hacerPedido(arrayProd, opcionCarta) {
-    switch (opcionCarta) {
-        case 1:
-            pedido.push(arrayProd[0]);
-            break;
-        case 2:
-            pedido.push(arrayProd[1]);
-            break;
-        case 3:
-            pedido.push(arrayProd[2]);
-            break;
-        default:
-            alert("estimado usuari@ por favor Seleccione una opcion valida");
-            agregarPedido();
-            break;
-    }
-}
-
-function modificarPedido() {
-    if (pedido.length > 0) {
-        let opcionEliminar = 0;
-        let productosPedidos = 'Seleccione la opción que desea eliminar del Pedido \n'
-        for (let i = 0; i < pedido.length; i++) {
-            productosPedidos = productosPedidos + (i + 1) + ' - ' + pedido[i].producto + ' ............ ' + pedido[i].precio + '\n'
-        }
-        opcionEliminar = parseInt(prompt(productosPedidos));
-
-        if (opcionEliminar > 0 && opcionEliminar <= pedido.length) {
-            let productoEliminado = pedido[opcionEliminar - 1];
-            pedido.splice((opcionEliminar - 1), 1);
-            alert(productoEliminado.producto + ', se ha eliminado correctamente del pedido');
-        } else {
-            alert('estimado usuari@, usted no ha seleccionado un producto valido para eliminar');
-            modificarPedido();
-        }
-
-
-    } else {
-        alert("estimado usuari@, usted no ha realizado ningun pedido aun");
-    }
-
-}

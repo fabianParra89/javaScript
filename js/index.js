ed87878c69caadf58c200cd72efc09a2faf36089
fabian parra <fabi_8931@hotmail.com>
2023-07-10 21:43:40 -0500

    estructurando el codigo js en rchivos por funcionalidad

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+import { Producto, inicializarProductos, filtroCartaProducto, cantidadProductosPedido, renderValorPedido, sumarPedido } from "./clases.js";
 
 let opcionBienvenida = 0;
 let confirmaPedido = false;
@@ -8,41 +9,10 @@ let idProducto = 1;
 
 const arrayCartaRestaurante = [];
 
-class Producto {
-    constructor(id, tipoProducto, producto, precio, src) {
-        this.id = id;
-        this.tipoProducto = tipoProducto;
-        this.producto = producto;
-        this.precio = precio;
-        this.src = src;
-    }
-}
 
-const entrada1 = new Producto(idProducto++, "entrada", "CANASTA DE EMPANADAS", 8000, "./img/empanadas.png");
-arrayCartaRestaurante.push(entrada1);
-const entrada2 = new Producto(idProducto++, "entrada", "CHICHARRONES APANADOS", 9000, "./img/chicharrones.png");
-arrayCartaRestaurante.push(entrada2);
-const entrada3 = new Producto(idProducto++, "entrada", "TOTOPOS", 9000, "./img/totopos.png");
-arrayCartaRestaurante.push(entrada3);
-const entrada4 = new Producto(idProducto++, "entrada", "CHINCHULINES", 8000, "./img/chinchulines.png");
-arrayCartaRestaurante.push(entrada4);
-const rapida1 = new Producto(idProducto++, "rapida", "HAMBURGUESA SENCILLA", 15000, "./img/hamburguesa.png");
-arrayCartaRestaurante.push(rapida1);
-const rapida2 = new Producto(idProducto++, "rapida", "HOT DOG", 13000, "./img/hotDog.png");
-arrayCartaRestaurante.push(rapida2);
-const rapida3 = new Producto(idProducto++, "rapida", "SALCHIPAPA", 12000, "./img/salchipapa.png");
-arrayCartaRestaurante.push(rapida3);
-const bebida1 = new Producto(idProducto++, "bebida", "LIMONADA", 4000, "./img/limonada.png");
-arrayCartaRestaurante.push(bebida1);
-const bebida2 = new Producto(idProducto++, "bebida", "COCA COLA", 3500, "./img/cocacola.png");
-arrayCartaRestaurante.push(bebida2);
-const bebida3 = new Producto(idProducto++, "bebida", "CERVEZA", 5000, "./img/cerveza.png");
-arrayCartaRestaurante.push(bebida3);
-
+inicializarProductos(arrayCartaRestaurante, idProducto)
 cantidadProductosPedido(arrayPedido);
 renderValorPedido(arrayPedido);
-
-
 
 const buttonEntrada = document.querySelector("#button-entrada");
 buttonEntrada.addEventListener("click", () => {
@@ -127,11 +97,11 @@ function renderPedido(arrayPedido) {
 
     if (arrayPedido.length > 0) {
         console.log("Array pedido True")
-    }else{
+    } else {
         console.log("Array pedido False")
     }
-    arrayPedido.length > 0 ?  tituloTipoProducto.innerText = `Su pedido se confirmo correctamente, el valor total a pagar es de ${sumarPedido(arrayPedido)}`: tituloTipoProducto.innerText = `Estimado cliente usted aun no ha realizado ningun pedido`;
-    
+    arrayPedido.length > 0 ? tituloTipoProducto.innerText = `Su pedido se confirmo correctamente, el valor total a pagar es de ${sumarPedido(arrayPedido)}` : tituloTipoProducto.innerText = `Estimado cliente usted aun no ha realizado ningun pedido`;
+
     contenedorTitulo.appendChild(tituloTipoProducto);
     console.log(arrayPedido);
 
@@ -172,7 +142,6 @@ function selectProductos(tipoProducto) {
 }
 
 function agregarPedido(producto) {
-    // producto.idPedido = idPedido++;
     arrayPedido.push(producto);
     localStorage.setItem("pedido", JSON.stringify(arrayPedido))
     cantidadProductosPedido(arrayPedido);
@@ -180,9 +149,7 @@ function agregarPedido(producto) {
 }
 
 function eliminarPedido(prodcuto, arrayPedido) {
-    console.log(prodcuto);
     let index = arrayPedido.indexOf(prodcuto);
-    console.log(index);
     index != -1 && arrayPedido.splice(index, 1);
     localStorage.removeItem("pedido");
     localStorage.setItem("pedido", JSON.stringify(arrayPedido));
@@ -191,31 +158,8 @@ function eliminarPedido(prodcuto, arrayPedido) {
     renderValorPedido(arrayPedido);
 }
 
-function cantidadProductosPedido(arrayPedido) {
-    const cantidadProductos = document.querySelector("#cantidad-productos");
-    cantidadProductos.innerHTML = "";
-    cantidadProductos.innerText = arrayPedido.length;
-}
-
-function renderValorPedido(arrayPedido) {
-    const valorPedido = document.querySelector("#valor-pedido");
-    valorPedido.innerHTML = "";
-    valorPedido.innerHTML = "$" + sumarPedido(arrayPedido);
-}
 
 
-function sumarPedido(arrayPedido) {
-    const precioPedido = arrayPedido.reduce((prev, current) => prev + current.precio, 0);
-    return precioPedido;
-}
 
 
-function filtroCartaProducto(array, tipoProducto) {
-    const cartaTipoProdFiltrado = array.filter(
-        (el) => {
-            return el.tipoProducto === tipoProducto;
-        }
-    )
-    return cartaTipoProdFiltrado;
-}
 
